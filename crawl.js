965604fda88856b6e4dca2e33a82a1f85d5b50b2
Zhu <artixzhu@gmail.com>
2019-06-05 19:58:13 -0400

    create two layer generators

---
+++ crawl.js
@@ -0,0 +1,209 @@
+const axios = require('axios');
+const FormData = require('form-data');
+const cheerio = require('cheerio');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+let idIterator = require('./idGenerator');
+const config = require('./configuration.json');
+const url = 'https://egov.uscis.gov/casestatus/mycasestatus.do';
+
+const { TIMEOUT_NO_BAN, CONCUR_THREAD, SLEEP_INTERVAL, SLEEP_PERIOD } = config;
+// const q = queue(function(payload, callback) {
+
+//     const bodyFormData = new FormData()
+//     bodyFormData.append('appReceiptNum', payload.id)
+//     bodyFormData.append('caseStatusSearchBtn', 'CHECK+STATUS')
+
+//     axios({
+//         method: 'POST',
+//         url: url,
+//         data: bodyFormData,
+//         config: { headers: {'Content-Type': 'application/x-www-form-urlencoded' }}
+//     }).then(res => {
+//         console.log(res)
+//         callback()
+//     }).catch(err => {
+//         console.log(err)
+//     }, 2)
+// })
+
+function fetchResult(id) {
+	const bodyFormData = new FormData();
+	bodyFormData.append('appReceiptNum', id);
+	bodyFormData.append('caseStatusSearchBtn', 'CHECK+STATUS');
+
+	return axios({
+		method: 'POST',
+		url: url,
+		data: bodyFormData,
+		config: { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+	}).then((res) => res.data || null);
+}
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+db.defaults({ cptVT: { records: [], invalidRecords: [], currentIdx: 0 } }).write();
+
+const whereILeft = db.get('cptVT').get('currentIdx').value();
+
+function writeMessages(msgObjs) {
+	if (Object.keys(msgObjs).length > 0) {
+		db.get('cptVT').get('records').push(...msgObjs).write();
+	}
+}
+
+function writeInvalidKeys(keys) {
+	if (keys.length > 0) {
+		db.get('cptVT').get('invalidRecords').push(...keys).write();
+	}
+}
+
+const snooze = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+let interval;
+
+function startInterval() {
+	interval = setInterval(async function() {
+		console.log('!!!!!!!start sleep!!!!!!!!!!!!!!!!!');
+		// await snooze(SLEEP_PERIOD*1000)
+		sleepThread(SLEEP_PERIOD);
+		console.log('!!!!!!!finish sleep!!!!!!!!!!!!!!!!!');
+	}, SLEEP_INTERVAL * 1000);
+}
+function cleanInterval() {
+	clearInterval(interval);
+}
+
+function msleep(n) {
+	Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
+}
+function sleepThread(n) {
+	msleep(n * 1000);
+}
+
+(async function main() {
+	let banned = false;
+
+	idIterator = idIterator(whereILeft);
+	let currentIt = idIterator.next();
+	startInterval();
+
+	while (!currentIt.done && !banned) {
+		//  const res = await fetchResult(stringID)
+		let reqAry = [],
+			ids = [],
+			msgs = [],
+			validMsgs = {},
+			invalidIds = [];
+
+		let subIdIterator = currentIt.value(),
+			subIt = subIdIterator.next();
+		while (!subIt.done && !banned) {
+			let counter = 0;
+			while (counter < CONCUR_THREAD) {
+				await snooze(TIMEOUT_NO_BAN);
+				const { stringID } = subIt.value;
+				reqAry.push(fetchResult(stringID));
+				ids.push(stringID);
+
+				subIt = subIdIterator.next();
+				counter++;
+			}
+
+			let htmls = [];
+			try {
+				htmls = await Promise.all(reqAry);
+			} catch (err) {
+				console.log(err);
+			}
+
+			// console.log(htmls)
+
+			for (let html of htmls) {
+				console.log(html);
+				const $ = cheerio.load(html);
+				if ($('label[for=accessviolation]').text()) {
+					console.log($('label[for=accessviolation]').text().trim());
+					banned = true;
+					break;
+				}
+				const message = $(
+					'body > div.main-content-sec.pb40 > form > div > div.container > div > div > div.col-lg-12.appointment-sec.center > div.rows.text-center > h1'
+				).text();
+				msgs.push(message);
+			}
+
+			// drop current message stack if banned
+
+			if (!banned) {
+				//traverse messages got from website
+				for (let i = 0; i < msgs.length; i++) {
+					if (!!msgs[i]) {
+						validMsgs[ids[i]] = msgs[i];
+					} else {
+						invalidIds.push(ids[i]);
+					}
+				}
+
+				// write data store it somewhere
+				if (Object.keys(validMsgs).length) {
+					writeMessages(validMsgs);
+				}
+				if (invalidIds.length) {
+					writeInvalidKeys(invalidIds);
+				}
+
+				db.get('cptVT').update('currentIdx', (n) => n + CONCUR_THREAD).write();
+			}
+		}
+
+		if (!banned) {
+			currentIt = idIterator.next();
+		}
+	}
+
+	cleanInterval();
+})();
+
+// for(let id of idIterator) {
+//     q.push({id}, function(err) {
+//         console.log()
+//     })
+// }
+
+// do{
+//     let currentIt = idIterator.next()
+//     let counter = 0
+
+//     while(counter < CONCUR_THREAD && !currentIt.done) {
+
+//         const bodyFormData = new FormData()
+//         bodyFormData.append('appReceiptNum', id)
+//         bodyFormData.append('caseStatusSearchBtn', 'CHECK+STATUS')
+
+//         axios({
+//             method: 'POST',
+//             url: url,
+//             data: bodyFormData,
+//             config: { headers: {'Content-Type': 'application/x-www-form-urlencoded' }}
+//         }).then(res => {
+//             console.log(res)
+//             callback()
+//         }).catch(err => {
+//             console.log(err)
+//         })
+//     }
+
+// } while(!currentIt.done)
+
+// // assign a callback
+// q.drain(function() {
+//     console.log('all items have been processed');
+// });
+
+// // assign an error callback
+// q.error(function(err, task) {
+//     console.error('task experienced an error');
+// });
--- idGenerator.js
+++ idGenerator.js
@@ -17,9 +17,15 @@ function* idGenerator(start = 0) {
 
 		skipTo = yield {
 			stringID: prefix + str,
-            range: str.slice(0, rangeDigits),
-            percentInRange: idx%(Number("1".padEnd(exampleSurfix.length - rangeDigits + 1)))/Number("1".padEnd(exampleSurfix.length - rangeDigits + 1)),
-			nextRangeStarts: prefix + String(Number(str.slice(0, rangeDigits)) + 1).padStart(rangeDigits, '0').padEnd(exampleSurfix.length, '0')
+			range: str.slice(0, rangeDigits),
+			percentInRange:
+				(idx % Number('1'.padEnd(exampleSurfix.length - rangeDigits + 1))) /
+				Number('1'.padEnd(exampleSurfix.length - rangeDigits + 1)),
+			nextRangeStarts:
+				prefix +
+				String(Number(str.slice(0, rangeDigits)) + 1)
+					.padStart(rangeDigits, '0')
+					.padEnd(exampleSurfix.length, '0')
 		};
 
 		if (typeof skipTo == 'string') {
@@ -30,31 +36,39 @@ function* idGenerator(start = 0) {
 	}
 }
 
-function *idGenerator2(start = 0) {
-	if (typeof start == 'string') {
-		start = Number(start.slice(start.length - exampleSurfix.length));
+function* idGenerator2(start = 0) {
+	// if (typeof start == 'string') {
+	// 	idx2.splice(0, prefix.length)
+	// }
+	let idx1 = 0,
+		idx2 = 0,
+		skipTo;
+
+	if (start) {
+		(idx1 = Number(start.slice(prefix.length, prefix.length + rangeDigits))),
+			(idx2 = Number(start.slice(prefix.length + rangeDigits))),
+			(skipTo = 0);
 	}
+	const range1 = Number('1' + '0'.repeat(rangeDigits)),
+		range2 = Number('1' + '0'.repeat(exampleSurfix.length - rangeDigits));
 
-	let idx = start,
-		skipTo = 0;
+	while (idx1 < range1) {
+		const str1 = String(idx1).padStart(rangeDigits, '0');
 
-	while (idx < Number('1' + '0'.repeat(rangeDigits))) {
-		const str1 = String(idx).padStart(rangeDigits, '0');
-
-		skipTo = yield function *subIdGenerator() {
-            for(let i = 0; i<Number('1' + '0'.repeat(exampleSurfix.length - rangeDigits)); i++) {
-                console.log()
-                const str2 = String(i).padStart('0')
-                yield (str1 + str2)
-            }
-        }
+		skipTo = yield function* subIdGenerator() {
+			while (idx2 < range2) {
+				const str2 = String(idx2).padStart(exampleSurfix.length - rangeDigits, '0');
+				yield { stringID: prefix + str1 + str2, percentage: idx2 / range2 };
+				idx2++;
+			}
+		};
 
 		// if (typeof skipTo == 'string') {
 		// 	skipTo = skipTo ? Number(skipTo.slice(skipTo.length - exampleSurfix.length)) : 0;
 		// }
-
-		idx = skipTo || idx + 1;
+		idx2 = 0;
+		idx1 = skipTo || idx1 + 1;
 	}
 }
 
-module.exports = idGenerator;
+module.exports = idGenerator2;
