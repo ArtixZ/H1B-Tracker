d7f4ce1ec2327b839aafa60ada1ea2799283f911
ArtixZ <artixzhu@gmail.com>
2019-07-18 20:30:58 -0400

    remove redundant generator

--- idGenerator.js
+++ idGenerator.js
@@ -4,37 +4,7 @@ const exampleSurfix = '6652149';
 
 const rangeDigits = 3;
 
-function* idGenerator(start = 0) {
-	if (typeof start == 'string') {
-		start = Number(start.slice(start.length - exampleSurfix.length));
-	}
-
-	let idx = start,
-		skipTo = 0;
-
-	while (idx < Number('1' + '0'.repeat(exampleSurfix.length))) {
-		const str = String(idx).padStart(exampleSurfix.length, '0');
-
-		skipTo = yield {
-			stringID: prefix + str,
-			range: str.slice(0, rangeDigits),
-			percentInRange:
-				(idx % Number('1'.padEnd(exampleSurfix.length - rangeDigits + 1))) /
-				Number('1'.padEnd(exampleSurfix.length - rangeDigits + 1)),
-			nextRangeStarts:
-				prefix +
-				String(Number(str.slice(0, rangeDigits)) + 1)
-					.padStart(rangeDigits, '0')
-					.padEnd(exampleSurfix.length, '0')
-		};
-
-		if (typeof skipTo == 'string') {
-			skipTo = skipTo ? Number(skipTo.slice(skipTo.length - exampleSurfix.length)) : 0;
-		}
-
-		idx = skipTo || idx + 1;
-	}
-}
+const maxNumberSegment = '67xxxxx';
 
 function* idGenerator2(start = 0) {
 	// if (typeof start == 'string') {
@@ -45,11 +15,15 @@ function* idGenerator2(start = 0) {
 		skipTo;
 
 	if (start && typeof start == 'string') {
-		(idx1 = Number(start.slice(prefix.length, prefix.length + rangeDigits))),
-			(idx2 = Number(start.slice(prefix.length + rangeDigits))),
-			(skipTo = 0);
+		(idx1 = Number(
+			start.slice(prefix.length, prefix.length + rangeDigits)
+		)), (idx2 = Number(
+			start.slice(prefix.length + rangeDigits)
+		)), (skipTo = 0);
 	} else if (start && typeof start == 'number') {
-		const divisor = Number('1' + '0'.repeat(exampleSurfix.length - rangeDigits));
+		const divisor = Number(
+			'1' + '0'.repeat(exampleSurfix.length - rangeDigits)
+		);
 		idx1 = Math.floor(start / divisor);
 		idx2 = start % divisor;
 		skipTo = 0;
@@ -62,7 +36,10 @@ function* idGenerator2(start = 0) {
 
 		skipTo = yield function* subIdGenerator() {
 			while (idx2 < range2) {
-				const str2 = String(idx2).padStart(exampleSurfix.length - rangeDigits, '0');
+				const str2 = String(idx2).padStart(
+					exampleSurfix.length - rangeDigits,
+					'0'
+				);
 				yield {
 					stringID: prefix + str1 + str2,
 					percentage: idx2 / range2,
