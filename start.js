46018dfb9c1d2b2702640e00f32ca6f22b248e26
Don <artixzhu@gmail.com>
2019-07-31 21:53:20 -0400

    start.js: auto restart proxy instance when banned.

--- start.js
+++ start.js
@@ -5,10 +5,12 @@ const proxyConfig = require('./conf.json');
 
 let banned = false;
 
+let proxyProcess, crawlProcess;
+
 function spawnProxy() {
 	let notCrawling = true;
 
-	const proxyProcess = spawn('scrapoxy', [ 'start', 'conf.json', '-d' ]);
+	proxyProcess = spawn('scrapoxy', [ 'start', 'conf.json', '-d' ]);
 	proxyProcess.stdout.on('data', (data) => {
 		console.log(`stdout: ${data}`);
 	});
@@ -27,26 +29,41 @@ function spawnProxy() {
 }
 
 function spawnCrawl() {
-	const crawlProcess = spawn('node', [ 'crawl.js' ]);
+	crawlProcess = spawn('node', [ 'crawl.js' ]);
 	crawlProcess.stdout.on('data', (data) => {
 		console.log(`stdout: ${data}`);
 		if (String(data).search('It was reported to us that your IP address') >= 0) {
+			banned = true;
+
 			axios
 				.get('http://localhost:8889/api/instances', {
 					headers: { Authorization: new Buffer(proxyConfig.commander.password).toString('base64') }
 				})
 				.then((res) => res.data)
 				.then((data) => {
-					const stopPromises = data.map((element) => {
-						axios.post('http://localhost:8889/api/instances/stop', {
-							headers: { Authorization: new Buffer(proxyConfig.commander.password).toString('base64') },
-							body: {
+					const stopPromises = data.map((element) =>
+						axios.post(
+							'http://localhost:8889/api/instances/stop',
+							{
 								name: element.name
+							},
+							{
+								headers: {
+									Authorization: new Buffer(proxyConfig.commander.password).toString('base64')
+								}
 							}
-						});
-					});
-					Promise.all(stopPromises).then((res) => {
-						console.log(res);
+						)
+					);
+					Promise.all(stopPromises).then((results) => {
+						const success = results.reduce((pre, cur) => cur.status == 200 && pre, true);
+						if (success) {
+							// wait for 2 mins and restart the crawl process
+							setTimeout(() => {
+								spawnCrawl();
+							}, 120000);
+						} else {
+							console.log('some instance is not stopped correctly!!');
+						}
 					});
 				})
 				.catch((err) => {
